32ded25a7c5b2f2e8267cf056efe78d6cfa33d01
MaggieMima <48593629+MaggieMima@users.noreply.github.com>
2020-03-16 20:12:43 +0100

    Initial commit

---
+++ xkocim05_xondru16.sql
@@ -0,0 +1,13 @@
+/**
+* @file xkocim05_xondru16.sql
+*
+* @brief IDS project, part 2  - Svet mágie
+*        SQL script for creating basic object of database's scheme 
+*
+* @authors Martin Koči          <xkocim05@stud.fit.vutbr.cz> 
+*          Magdaléna Ondrušková <xondru16@stud.fit.vutbr.cz>
+*
+* @date  16/03/2020
+*/
+
+
